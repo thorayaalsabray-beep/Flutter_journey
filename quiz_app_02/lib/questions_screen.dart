109305e0df5b32a43061fd7a177fdbfb555d94ab
thorayaalsabray <thorayaalsabray@gmail.com>
2026-06-01 07:02:12 +0300

    Quiz app:new update

--- quiz_app_02/lib/questions_screen.dart
+++ quiz_app_02/lib/questions_screen.dart
@@ -4,14 +4,36 @@ class QuiztionsScreen extends StatefulWidget {
   const QuiztionsScreen({super.key});
   @override
   State<QuiztionsScreen> createState() {
-  return _QuiztionsScreen();
-}
-}
-class _QuiztionsScreen extends State<QuiztionsScreen>{
-   @override
-  Widget build(constant){
-    return const Text('QuestionScreen');
+    return _QuiztionsScreenState();
   }
 }
 
-
+class _QuiztionsScreenState extends State<QuiztionsScreen> {
+  @override
+  Widget build(constant) {
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('The Questions...',style: TextStyle(color:Color.fromARGB(255, 54, 53, 53),),),
+          const SizedBox(
+            height: 30,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 1'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 2'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 3'),
+          ),
+        ],
+      ),
+    );
+  }
+}
